17779b43af74483a0d2741481631089ea8fa7573
Tumay Ozdemir <tumayozdemir96@gmail.com>
2021-04-30 10:26:02 -0400

    Add project files.

---
+++ Project1/main.cpp
@@ -0,0 +1,224 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cfloat>
+
+#include "gluvi.h"
+#include "fluidsim.h"
+#include "openglutils.h"
+#include "array2_utils.h"
+
+#include "fluidsim.cpp"
+#include "gluvi.cpp"
+#include "openglutils.cpp"
+
+using namespace std;
+
+
+//Try changing the grid resolution
+int grid_resolution = 30;
+float timestep = 0.001f;
+
+//Display properties
+bool draw_grid = true;
+bool draw_particles = true;
+bool draw_velocities = true;
+bool draw_boundaries = true;
+
+float grid_width = 1;
+
+FluidSim sim;
+
+//Gluvi stuff
+//-------------
+Gluvi::PanZoom2D cam(-0.1f, -0.35f, 1.2f);
+double oldmousetime;
+Vec2f oldmouse;
+void display();
+void mouse(int button, int state, int x, int y);
+void drag(int x, int y);
+void timer(int junk);
+
+
+//Boundary definition - several circles in a circular domain.
+
+Vec2f c0(0.5f,0.5f), c1(0.7f,0.5f), c2(0.3f,0.35f), c3(0.5f,0.7f);
+float rad0 = 0.4f,  rad1 = 0.1f,  rad2 = 0.1f,   rad3 = 0.1f;
+
+float circle_phi(const Vec2f& position, const Vec2f& centre, float radius) {
+   return (dist(position,centre) - radius);
+}
+
+float boundary_phi(const Vec2f& position) {
+   float phi0 = -circle_phi(position, c0, rad0);
+   //float phi1 = circle_phi(position, c1, rad1);
+   //float phi2 = circle_phi(position, c2, rad2);
+   //float phi3 = circle_phi(position, c3, rad3);
+   //return min(min(phi0,phi1),min(phi2,phi3));
+   return phi0;
+}
+
+//Draw velocity for each particle -- Tumay
+void draw_velocity() {
+    for (int i = 0; i < sim.particles.size(); ++i) {
+        Vec2f pos = sim.particles[i];
+        Vec2f vel = sim.get_velocity(sim.particles[i]);
+
+        float vel_norm = sqrt(pow(vel[0], 2) + pow(vel[1], 2));
+        Vec2f end = Vec2f(pos[0] + vel[0] / vel_norm / 20, pos[1] + vel[1] / vel_norm / 20);
+
+        glBegin(GL_POINTS);
+        glColor3f(1, 1, 1);
+        glVertex2f(end[0], end[1]);
+        glEnd();
+
+        glColor3f(1, 0, 0);
+        glBegin(GL_LINES);
+        glVertex2f(pos[0], pos[1]);
+        glVertex2f(end[0], end[1]);
+        glEnd();
+    }
+}
+
+
+
+//Main testing code
+//-------------
+int main(int argc, char **argv)
+{
+   
+   //Setup viewer stuff
+   Gluvi::init("Basic Fluid Solver with Vorticity", &argc, argv);
+   Gluvi::camera=&cam;
+   Gluvi::userDisplayFunc=display;
+   Gluvi::userMouseFunc=mouse;
+   Gluvi::userDragFunc=drag;
+   glClearColor(1,1,1,1);
+   
+   glutTimerFunc(1000, timer, 0);
+   
+   //Set up the simulation
+   sim.initialize(grid_width, grid_resolution, grid_resolution);
+   sim.set_boundary(boundary_phi);
+   
+   /*for(int i = 0; i < sqr(grid_resolution); ++i) {
+      float x = randhashf(i*2, 0,1);
+      float y = randhashf(i*2+1, 0,1);
+      Vec2f pt(x,y);
+      if(boundary_phi(pt) > 0)
+         sim.add_particle(pt);
+   }*/
+   float dx = grid_width / (float)grid_resolution;
+   for (int i = 0; i < grid_resolution + 1; i++) {
+       for (int j = 0; j < grid_resolution + 1; j++) {
+           float x = i * dx;
+           float y = j * dx;
+           if (0.4 <= x && x <= 0.7 && 0.4 <= y && y <= 0.7) {
+               sim.add_particle(Vec2f(x, y));
+           }           
+       }
+   }
+   //sim.advance(timestep);
+   Gluvi::run();
+
+   return 0;
+}
+
+
+void display(void)
+{
+  
+   if(draw_grid) {
+      glColor3f(0,0,0);
+      glLineWidth(1);
+      //draw_grid2d(Vec2f(0,0), sim.dx, sim.ni, sim.nj);  
+   }
+
+   if(draw_boundaries) {
+      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+      draw_circle2d(c0, rad0, 50); 
+      /*draw_circle2d(c1, rad1, 50); 
+      draw_circle2d(c2, rad2, 50); 
+      draw_circle2d(c3, rad3, 50); 
+      
+      //There's a bug, so draw one more(?)
+      draw_circle2d(c3, 0, 10);*/
+   }
+
+   if(draw_particles) {
+      glColor3f(0,0,0);
+      glPointSize(3);
+      draw_points2d(sim.particles);
+   }
+   
+   /*if(draw_velocities) {
+      for(int j = 0;j < sim.nj; ++j) for(int i = 0; i < sim.ni; ++i) {
+         Vec2f pos((i+0.5f)*sim.dx,(j+0.5f)*sim.dx);
+         draw_arrow2d(pos, pos + 0.01f*sim.get_velocity(pos), 0.1f*sim.dx);
+         //cout << "Vel x: " << sim.get_velocity(pos)[0] << "Vel y:" << sim.get_velocity(pos)[1] << endl;
+      }
+   }*/
+
+   draw_velocity();
+   /*glPointSize(5.0);
+   glBegin(GL_POINTS);
+   for (int i = 0; i <= sim.ni; i++) {
+       for (int j = 0; j < sim.nj; j++) {
+           
+           if (sim.vor(i, j) > 10)
+               glColor3f(1, 0, 0);
+           else if (sim.vor(i, j) > 5 && sim.vor(i, j) < 10)
+               glColor3f(0.66, 0, 0);
+           else if (sim.vor(i, j) < 5 && sim.vor(i,j) > 0)
+               glColor3f(0.33, 0, 0);
+           else if (sim.vor(i, j) < -10)
+               glColor3f(0, 0, 1);
+           else if (sim.vor(i, j) > -10 && sim.vor(i, j) < -5)
+               glColor3f(0, 0, 0.66);
+           else if (sim.vor(i, j) < 0 && sim.vor(i, j) > -5)
+               glColor3f(0, 0, 0.33);
+           else
+               glColor3f(0, 1, 0);
+           glVertex2f(i * sim.dx, j * sim.dx);
+       }
+   }
+   glEnd();*/
+
+}
+
+void mouse(int button, int state, int x, int y)
+{
+   Vec2f newmouse;
+   cam.transform_mouse(x, y, newmouse.v);
+   //double newmousetime=get_time_in_seconds();
+
+   oldmouse=newmouse;
+   //oldmousetime=newmousetime;
+}
+
+void drag(int x, int y)
+{
+   Vec2f newmouse;
+   cam.transform_mouse(x, y, newmouse.v);
+   //double newmousetime=get_time_in_seconds();
+
+   oldmouse=newmouse;
+   //oldmousetime=newmousetime;
+}
+
+void timer(int junk)
+{
+   sim.advance(timestep);
+
+   glutPostRedisplay();
+   glutTimerFunc(3000, timer, 0);
+
+}
+
+
+
+
+
---
+++ Project1/vorticity.h
@@ -0,0 +1,105 @@
+#include <iostream>
+
+#include "vec.h"
+#include "array2.h"
+
+using namespace std;
+const float PI = 3.14159265358979;
+const float SIGMA = 0.0001;
+
+Array2f gamma_init(float dx, int ni, int nj) {
+	Array2f gamma;
+	gamma.resize(ni, nj); 
+	gamma.set_zero();
+	for (int i = 0; i < ni; i++) {
+		for (int j = 0; j < nj; j++) {
+			float x = i * dx;
+			float y = j * dx;
+			if (0.4 <= x && x <= 0.7 && 0.4 <= y && y <= 0.7) {
+				gamma(i, j) = 2.0 * (y - x);
+			}
+			else
+				gamma(i, j) = 0;
+		}
+	}
+	return gamma;
+}
+float gauss_distr(Vec2f x) {
+	return 1 / (2 * PI * sqr(SIGMA)) * exp(-(sqr(x[0])+ sqr(x[1])) / (2 * sqr(SIGMA)));
+}
+
+float q_function(float x) {
+	return (1.0f - exp(-0.5 * sqr(x))) / (2.0f * PI);
+}
+
+Vec2f biot_savart(float dx, int ni, int nj, Vec2f pos, Array2f vor) {
+	Vec2f vel = Vec2f(0, 0);
+	for (int i = 0; i < ni; i++) {
+		for (int j = 0; j < nj; j++) {
+			Vec2f diff = pos - Vec2f(i * dx, j * dx);
+			diff[1] = - diff[1];
+			float distance = sqrt(pow(diff[0], 2) + pow(diff[1], 2));
+			if (distance != 0) {
+				vel -= diff * vor(i, j) * q_function(distance / SIGMA) / sqr(distance);
+			}
+		}
+	}
+	return vel;
+}
+
+Array2f vor_inversion_u(float dx, int ni, int nj, Array2f vor) {
+	Array2f u; 
+	u.resize(ni+1, nj);
+	u.set_zero();
+	for (int i = 0; i < ni; i++) {
+		for (int j = 0; j < nj; j++) {
+			u(i, j) = biot_savart(dx, ni, nj, Vec2f(i * dx, j * dx), vor)[0];
+		}
+	}
+	return u;
+}
+
+Array2f vor_inversion_v(float dx, int ni, int nj, Array2f vor) {
+	Array2f u;
+	u.resize(ni, nj+1);
+	u.set_zero();
+	for (int i = 0; i < ni; i++) {
+		for (int j = 0; j < nj; j++) {
+			u(i, j) = biot_savart(dx, ni, nj, Vec2f(i * dx, j * dx), vor)[1];
+		}
+	}
+	return u;
+}
+
+Array2f curl_2D(Array2f u, Array2f v, int ni, int nj, float dx) {
+	Array2f vor;
+	vor.resize(ni, nj);
+	vor.set_zero();
+	for (int i = 0; i < ni ; i++) {
+		for (int j = 0; j < nj ; j++) {
+			//curl : dv/dx - du/dy
+			//dv/dx = v(x+dx) - v(x-dx) /2dx
+			float dv_dx = 0;
+			float du_dy = 0;
+			if (i == 0) {
+				dv_dx = (v(i + 1, j) - 0) / dx * 0.5;
+			} else if (i == ni-1) {
+				dv_dx = (0 - v(i - 1, j)) / dx * 0.5;
+			} else{
+				dv_dx = (v(i + 1, j) - v(i - 1, j)) / dx * 0.5;
+			} 
+
+			if (j == 0) {
+				du_dy = (u(i, j + 1) - 0) / dx * 0.5;
+			} else if (j == nj-1) {
+				du_dy = (0 - u(i, j - 1)) / dx * 0.5;
+			} else {
+				du_dy = (u(i, j + 1) - u(i, j - 1)) / dx * 0.5;
+			}
+			
+			vor(i, j) = dv_dx - du_dy;
+		}
+	}
+	return vor;
+}
+
