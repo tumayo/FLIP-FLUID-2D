98ce5260845ce881edf4086ebd5a4a4b8eb37abc
Tumay Ozdemir <tumayozdemir96@gmail.com>
2021-04-30 10:46:20 -0400

    Update

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -15,12 +15,18 @@
 #include "gluvi.cpp"
 #include "openglutils.cpp"
 
+//Comment this to disable frame dumping
+#define MAKE_MOVIE
+
+
 using namespace std;
 
 
 //Try changing the grid resolution
 int grid_resolution = 30;
 float timestep = 0.001f;
+int pause_btw_frames_in_ms = 10;
+char img_file_path[]{ "C:/output/" };
 
 //Display properties
 bool draw_grid = true;
@@ -45,20 +51,20 @@ void timer(int junk);
 
 //Boundary definition - several circles in a circular domain.
 
-Vec2f c0(0.5f,0.5f), c1(0.7f,0.5f), c2(0.3f,0.35f), c3(0.5f,0.7f);
-float rad0 = 0.4f,  rad1 = 0.1f,  rad2 = 0.1f,   rad3 = 0.1f;
+Vec2f c0(0.5f, 0.5f), c1(0.7f, 0.5f), c2(0.3f, 0.35f), c3(0.5f, 0.7f);
+float rad0 = 0.4f, rad1 = 0.1f, rad2 = 0.1f, rad3 = 0.1f;
 
 float circle_phi(const Vec2f& position, const Vec2f& centre, float radius) {
-   return (dist(position,centre) - radius);
+    return (dist(position, centre) - radius);
 }
 
 float boundary_phi(const Vec2f& position) {
-   float phi0 = -circle_phi(position, c0, rad0);
-   //float phi1 = circle_phi(position, c1, rad1);
-   //float phi2 = circle_phi(position, c2, rad2);
-   //float phi3 = circle_phi(position, c3, rad3);
-   //return min(min(phi0,phi1),min(phi2,phi3));
-   return phi0;
+    float phi0 = -circle_phi(position, c0, rad0);
+    //float phi1 = circle_phi(position, c1, rad1);
+    //float phi2 = circle_phi(position, c2, rad2);
+    //float phi3 = circle_phi(position, c3, rad3);
+    //return min(min(phi0,phi1),min(phi2,phi3));
+    return phi0;
 }
 
 //Draw velocity for each particle -- Tumay
@@ -87,134 +93,148 @@ void draw_velocity() {
 
 //Main testing code
 //-------------
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-   
-   //Setup viewer stuff
-   Gluvi::init("Basic Fluid Solver with Vorticity", &argc, argv);
-   Gluvi::camera=&cam;
-   Gluvi::userDisplayFunc=display;
-   Gluvi::userMouseFunc=mouse;
-   Gluvi::userDragFunc=drag;
-   glClearColor(1,1,1,1);
-   
-   glutTimerFunc(1000, timer, 0);
-   
-   //Set up the simulation
-   sim.initialize(grid_width, grid_resolution, grid_resolution);
-   sim.set_boundary(boundary_phi);
-   
-   /*for(int i = 0; i < sqr(grid_resolution); ++i) {
-      float x = randhashf(i*2, 0,1);
-      float y = randhashf(i*2+1, 0,1);
-      Vec2f pt(x,y);
-      if(boundary_phi(pt) > 0)
-         sim.add_particle(pt);
-   }*/
-   float dx = grid_width / (float)grid_resolution;
-   for (int i = 0; i < grid_resolution + 1; i++) {
-       for (int j = 0; j < grid_resolution + 1; j++) {
-           float x = i * dx;
-           float y = j * dx;
-           if (0.4 <= x && x <= 0.7 && 0.4 <= y && y <= 0.7) {
-               sim.add_particle(Vec2f(x, y));
-           }           
-       }
-   }
-   //sim.advance(timestep);
-   Gluvi::run();
 
-   return 0;
+    //Setup viewer stuff
+    Gluvi::init("Basic Fluid Solver with Vorticity", &argc, argv);
+    Gluvi::camera = &cam;
+    Gluvi::userDisplayFunc = display;
+    Gluvi::userMouseFunc = mouse;
+    Gluvi::userDragFunc = drag;
+    glClearColor(1, 1, 1, 1);
+
+    glutTimerFunc(1000, timer, 0);
+
+    //Set up the simulation
+    sim.initialize(grid_width, grid_resolution, grid_resolution);
+    sim.set_boundary(boundary_phi);
+
+    /*for(int i = 0; i < sqr(grid_resolution); ++i) {
+       float x = randhashf(i*2, 0,1);
+       float y = randhashf(i*2+1, 0,1);
+       Vec2f pt(x,y);
+       if(boundary_phi(pt) > 0)
+          sim.add_particle(pt);
+    }*/
+    float dx = grid_width / (float)grid_resolution;
+    for (int i = 0; i < grid_resolution + 1; i++) {
+        for (int j = 0; j < grid_resolution + 1; j++) {
+            float x = i * dx;
+            float y = j * dx;
+            if (0.4 <= x && x <= 0.7 && 0.4 <= y && y <= 0.7) {
+                sim.add_particle(Vec2f(x, y));
+            }
+        }
+    }
+    //sim.advance(timestep);
+    Gluvi::run();
+
+    return 0;
 }
 
 
 void display(void)
 {
-  
-   if(draw_grid) {
-      glColor3f(0,0,0);
-      glLineWidth(1);
-      //draw_grid2d(Vec2f(0,0), sim.dx, sim.ni, sim.nj);  
-   }
 
-   if(draw_boundaries) {
-      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-      draw_circle2d(c0, rad0, 50); 
-      /*draw_circle2d(c1, rad1, 50); 
-      draw_circle2d(c2, rad2, 50); 
-      draw_circle2d(c3, rad3, 50); 
-      
-      //There's a bug, so draw one more(?)
-      draw_circle2d(c3, 0, 10);*/
-   }
+    if (draw_grid) {
+        glColor3f(0, 0, 0);
+        glLineWidth(1);
+        //draw_grid2d(Vec2f(0,0), sim.dx, sim.ni, sim.nj);  
+    }
 
-   if(draw_particles) {
-      glColor3f(0,0,0);
-      glPointSize(3);
-      draw_points2d(sim.particles);
-   }
-   
-   /*if(draw_velocities) {
-      for(int j = 0;j < sim.nj; ++j) for(int i = 0; i < sim.ni; ++i) {
-         Vec2f pos((i+0.5f)*sim.dx,(j+0.5f)*sim.dx);
-         draw_arrow2d(pos, pos + 0.01f*sim.get_velocity(pos), 0.1f*sim.dx);
-         //cout << "Vel x: " << sim.get_velocity(pos)[0] << "Vel y:" << sim.get_velocity(pos)[1] << endl;
-      }
-   }*/
+    if (draw_boundaries) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        draw_circle2d(c0, rad0, 50);
+        /*draw_circle2d(c1, rad1, 50);
+        draw_circle2d(c2, rad2, 50);
+        draw_circle2d(c3, rad3, 50);
 
-   draw_velocity();
-   /*glPointSize(5.0);
-   glBegin(GL_POINTS);
-   for (int i = 0; i <= sim.ni; i++) {
-       for (int j = 0; j < sim.nj; j++) {
-           
-           if (sim.vor(i, j) > 10)
-               glColor3f(1, 0, 0);
-           else if (sim.vor(i, j) > 5 && sim.vor(i, j) < 10)
-               glColor3f(0.66, 0, 0);
-           else if (sim.vor(i, j) < 5 && sim.vor(i,j) > 0)
-               glColor3f(0.33, 0, 0);
-           else if (sim.vor(i, j) < -10)
-               glColor3f(0, 0, 1);
-           else if (sim.vor(i, j) > -10 && sim.vor(i, j) < -5)
-               glColor3f(0, 0, 0.66);
-           else if (sim.vor(i, j) < 0 && sim.vor(i, j) > -5)
-               glColor3f(0, 0, 0.33);
-           else
-               glColor3f(0, 1, 0);
-           glVertex2f(i * sim.dx, j * sim.dx);
+        //There's a bug, so draw one more(?)
+        draw_circle2d(c3, 0, 10);*/
+    }
+
+    if (draw_particles) {
+        glColor3f(0, 0, 0);
+        glPointSize(3);
+        draw_points2d(sim.particles);
+    }
+
+    /*if(draw_velocities) {
+       for(int j = 0;j < sim.nj; ++j) for(int i = 0; i < sim.ni; ++i) {
+          Vec2f pos((i+0.5f)*sim.dx,(j+0.5f)*sim.dx);
+          draw_arrow2d(pos, pos + 0.01f*sim.get_velocity(pos), 0.1f*sim.dx);
+          //cout << "Vel x: " << sim.get_velocity(pos)[0] << "Vel y:" << sim.get_velocity(pos)[1] << endl;
        }
-   }
-   glEnd();*/
+    }*/
+
+    draw_velocity();
+    /*glPointSize(5.0);
+    glBegin(GL_POINTS);
+    for (int i = 0; i <= sim.ni; i++) {
+        for (int j = 0; j < sim.nj; j++) {
+
+            if (sim.vor(i, j) > 10)
+                glColor3f(1, 0, 0);
+            else if (sim.vor(i, j) > 5 && sim.vor(i, j) < 10)
+                glColor3f(0.66, 0, 0);
+            else if (sim.vor(i, j) < 5 && sim.vor(i,j) > 0)
+                glColor3f(0.33, 0, 0);
+            else if (sim.vor(i, j) < -10)
+                glColor3f(0, 0, 1);
+            else if (sim.vor(i, j) > -10 && sim.vor(i, j) < -5)
+                glColor3f(0, 0, 0.66);
+            else if (sim.vor(i, j) < 0 && sim.vor(i, j) > -5)
+                glColor3f(0, 0, 0.33);
+            else
+                glColor3f(0, 1, 0);
+            glVertex2f(i * sim.dx, j * sim.dx);
+        }
+    }
+    glEnd();*/
 
 }
 
 void mouse(int button, int state, int x, int y)
 {
-   Vec2f newmouse;
-   cam.transform_mouse(x, y, newmouse.v);
-   //double newmousetime=get_time_in_seconds();
+    Vec2f newmouse;
+    cam.transform_mouse(x, y, newmouse.v);
+    //double newmousetime=get_time_in_seconds();
 
-   oldmouse=newmouse;
-   //oldmousetime=newmousetime;
+    oldmouse = newmouse;
+    //oldmousetime=newmousetime;
 }
 
 void drag(int x, int y)
 {
-   Vec2f newmouse;
-   cam.transform_mouse(x, y, newmouse.v);
-   //double newmousetime=get_time_in_seconds();
+    Vec2f newmouse;
+    cam.transform_mouse(x, y, newmouse.v);
+    //double newmousetime=get_time_in_seconds();
 
-   oldmouse=newmouse;
-   //oldmousetime=newmousetime;
+    oldmouse = newmouse;
+    //oldmousetime=newmousetime;
 }
 
 void timer(int junk)
 {
-   sim.advance(timestep);
+    sim.advance(timestep);
 
-   glutPostRedisplay();
-   glutTimerFunc(3000, timer, 0);
+
+#ifdef MAKE_MOVIE
+    static int frame = 0;
+    frame++;
+
+    char* sgifileformat;
+    sgifileformat = new char[strlen(img_file_path) + 50];
+
+    sprintf(sgifileformat, "%s/screenshot%%04d.sgi", img_file_path);
+    Gluvi::sgi_screenshot(sgifileformat, frame);
+
+    delete[] sgifileformat;
+#endif
+
+    glutPostRedisplay();
+    glutTimerFunc(pause_btw_frames_in_ms, timer, 0);
 
 }
 
